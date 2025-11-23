cbefda9d5607dac8b1f53f89a6269efa4449e4b6
Akesh Soni <akesh@Akeshs-MacBook.local>
2025-11-23 18:39:07 +0530

    Fix supabase config

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://ehcecqguqsenhstbuquy.supabase.co
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoY2VjcWd1cXNlbmhzdGJ1cXV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4OTc3MDEsImV4cCI6MjA3OTQ3MzcwMX0.fIwGkFlruoaUfrJIgJWhUWLLxfZi8UxGSVYCyKF2290
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
